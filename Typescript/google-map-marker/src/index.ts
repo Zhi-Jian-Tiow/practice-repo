1f4a7f931b2aa874d6840936760bc2e23295a11f
Zhi Jian Tiow <s2437662@ed.ac.uk>
2024-05-11 16:59:15 +0100

    TypeScript practice google-map-marker

---
+++ Typescript/google-map-marker/src/index.ts
@@ -0,0 +1,7 @@
+import { User } from "./User";
+import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
+
+const customMap = new CustomMap("map");
+customMap.addMarker(new User());
+customMap.addMarker(new Company());
